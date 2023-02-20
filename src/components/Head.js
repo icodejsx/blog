bc2c8d747e8ad2d655cda1b9c0cf2253ad3ffda3
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-02-20 20:45:54 +0100

    building

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
-// import { ArrowLeft } from "phosphor-react";
+import {
+  ArrowLeft,
+  ArrowRight,
+  WhatsappLogo,
+  FacebookLogo,
+  InstagramLogo,
+  Basketball,
+  User,
+  TiktokLogo,
+  List,
+  Cloud,
+} from "phosphor-react";
 
 const Head = () => {
   const el = useRef(null);
@@ -34,15 +45,36 @@ const Head = () => {
   }, []);
 
   return (
-    <div className="hidden md:flex flex-row justify-start px-10 bg-yellow-400 gap-2">
-      <div className="flex flex-row justify-center items-start ">
+    <div className="hidden md:flex flex-row px-10 bg-yellow-400 gap-2 justify-between ">
+      <div className="flex flex-row justify-center items-start  w-50">
         <div className="py-2">🕧{current}</div>
         <span className="bg-red-800 text-white inline p-2">Braking News</span>
         <span className=".mov py-2">
           <span style={{ whiteSpace: "pre" }} ref={el} />
         </span>
       </div>
-      <div></div>
+      <div className="md:flex justify-end p-2 gap-3">
+        <div className="flex felx-row gap-2">
+          <div className="border border-spacing-1 shadow-md ">
+            <ArrowLeft size={20} />
+          </div>
+          <div className="border border-spacing-1 shadow-md ">
+            <ArrowRight size={20} />
+          </div>
+        </div>
+        <div clas>
+          <FacebookLogo size={20} />
+        </div>
+
+        <WhatsappLogo size={20} />
+        <TiktokLogo size={20} />
+        <Cloud size={20} />
+        <InstagramLogo size={20} />
+        <User size={20} />
+
+        <Basketball size={20} />
+        <List size={20} />
+      </div>
     </div>
   );
 };
