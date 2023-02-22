7f02c9f7f4ec73069cef9f2db13fc6ca4c6df38f
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-02-22 11:32:43 +0100

    done with the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -143,7 +143,7 @@ function Nav() {
                 <input
                   type="search"
                   id="default-search"
-                  className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="block w-full p-4 pl-10 text-sm  bg-black  d "
                   placeholder="Search "
                   required
                 />
