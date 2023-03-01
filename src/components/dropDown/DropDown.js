663b63227abbc6bb08a286061137e31f55bd93f1
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-03-01 11:00:10 +0100

    Api curently working at the navbar

--- src/components/dropDown/DropDown.js
+++ src/components/dropDown/DropDown.js
@@ -1,28 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BlogList } from "../../Data/BlogData";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const DropDown = ({ typ = "Newss" }) => {
   const type = typ;
   console.log(type);
 
-  const [post, setPost] = useState();
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const options = {
+        method: "GET",
+        url: "https://api.newscatcherapi.com/v2/search",
+        params: { q: "travel", lang: "en", sort_by: "relevancy", page: "1" },
+        headers: {
+          "x-api-key": "UBTpdFLawMbbfg9sixNULyNVAkHrFPa7TGKH4aA-cy4",
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        setNews(response.data.articles);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchNews();
+  }, []);
 
   return (
     <div>
       <section className="text-gray-600 body-font hidden lg:flex ">
         <div className="container px-10 py-5 mx-auto items-center flex justify-center ">
           <div className="flex flex-wrap flex-row -m-4  ">
-            {BlogList.slice(0, 5).map((blog, index) => (
+            {news.slice(0, 5).map((blog, index) => (
               <div
                 className="p-1 md:w-1/5 flex  flex-row  items-center  "
                 key={index}
               >
                 <div className="h-full  overflow-hidden ">
-                  <Link to={`/posts/${blog.id}`}>
+                  <Link to={`/posts/${blog._id}`}>
                     <img
                       className="lg:h-40 md:h-20 w-full object-cover object-center"
-                      src={blog.img}
+                      src={blog.media}
                       alt="blog"
                     />
                   </Link>
