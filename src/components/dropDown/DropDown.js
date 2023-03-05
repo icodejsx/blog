eee60a049670426c9a5d014c4e097e2ad0906a5f
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-03-05 19:46:31 +0100

    sunday

--- src/components/dropDown/DropDown.js
+++ src/components/dropDown/DropDown.js
@@ -73,7 +73,7 @@ const DropDown = ({ category }) => {
                     <img
                       className="lg:h-40 md:h-20 w-full object-cover object-center"
                       src={blog.img}
-                      alt="blog"
+                      alt="blo"
                     />
                   </Link>
                   <div className="flex flex-col justify-start items-start text-gray-400 ">
