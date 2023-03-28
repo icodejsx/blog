335d2a14b556968b50ea6e0e95a8f3e859ff1d44
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-03-28 15:57:10 +0100

    working on the last part

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { TwitterLogo } from "phosphor-react";
 const Footer = () => {
   return (
     <div className="bg-black ">
-      <div className="md:grid gap-3  md:grid-cols-4 w-full lg:p-20 p-10 flex flex-col ">
+      <div className="md:grid gap-3  md:grid-cols-4 w-full lg:px-20 p-10 flex flex-col ">
         <div className="col-span-1">
           <div className="">
             <p className="border-b-4 pb-3 border-green-500  inline-block">
@@ -262,6 +262,46 @@ const Footer = () => {
             </Link>
           </div>
         </div>
+      </div>
+
+      <div className="flex md:px-20 p-10  gap-20">
+        <div className=" flex items-end">
+          <h1 className="text-7xl text-white font-bold">Jannah</h1>
+          <span className="text-4xl text-white">news</span>
+        </div>
+
+        <div className=" w-96 text-sm">
+          <p>
+            Jannah is a Clean Responsive WordPress Newspaper, Magazine, News and
+            Blog theme. Packed with options that allow you to completely
+            customize your website to your needs.
+          </p>
+        </div>
+
+        <div className="w-96">
+          {" "}
+          <div>
+            <label
+              for="first_name"
+              className="block mb-2 text-sm font-medium text-white"
+            ></label>
+            <input
+              type="text"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <p className="px-6 py-2 bg-blue-700 mt-2 text-white  flex items-center justify-center">
+            Subscribe
+          </p>
+        </div>
+      </div>
+
+      <div className="flex md:px-20 p-10  ">
+        <p>&copy;Copyright 2023, All Rights Reserved</p>
+        <div></div>
       </div>
     </div>
   );
