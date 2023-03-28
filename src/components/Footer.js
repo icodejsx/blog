98e123434344476b75b034653f1ce35c93886c8f
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-03-28 13:24:59 +0100

    last part

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="bg-black ">
-      <div className="grid gap-3  grid-cols-4 w-full p-20">
+      <div className="md:grid gap-3  md:grid-cols-4 w-full lg:p-20 p-10 flex flex-col ">
         <div className="col-span-1">
           <div className="">
             <p className="border-b-4 pb-3 border-green-500  inline-block">
               Most Viewed Post{" "}
             </p>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
             <Link to="">
               <div className=" hover:text-blue-600">
                 <p className="text-xs"> oct 29, 2019 </p>
@@ -41,15 +41,232 @@ const Footer = () => {
           </div>
         </div>
         <div className="col-span-1">
-          <div>
+          <div className="mb-2">
             <p className="border-b-4 pb-3  inline-block">Last Modified Posts</p>
           </div>
-          <div className="flex flex-col ">
-            <div></div>
+          <div className="flex flex-row gap-2 flex-wrap ">
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/post-1-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/05/11-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/#:~:text=Tags,a%20Clean%20Responsive"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/slide-31-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/slide-31-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/slide-31-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/post-1-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/post-1-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/post-1-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  src="https://jannah.tielabs.com/demo/wp-content/uploads/sites/8/2016/10/post-1-390x220.jpg"
+                  alt=""
+                  className="h-12"
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-span-1"></div>
-        <div className="col-span-1"></div>
+        <div className="col-span-1">
+          <div className="mb-2">
+            <p className="border-b-4 pb-3 border-green-500  inline-block">
+              Tag
+            </p>
+          </div>
+          <div className="flex flex-row gap-2 flex-wrap">
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                app
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                apple
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                Facebook
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                app
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                apple
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                Facebook
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                app
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                apple
+              </div>
+            </Link>
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                app
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                Facebook
+              </div>
+            </Link>
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                app
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                apple
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                Facebook
+              </div>
+            </Link>
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                app
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600 border  border-gray-400 p-1 px-2 inline-block">
+                apple
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="">
+            <p className="border-b-4 pb-3 border-green-500  inline-block">
+              Follow us
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <Link to="">
+              <div className=" hover:text-blue-600">
+                <p className="text-xs"> oct 29, 2019 </p>
+                <span className="font-bold text-white  hover:text-blue-600">
+                  After all is said and done, more is done
+                </span>
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600">
+                <p className="text-xs"> oct 29, 2019 </p>
+                <span className="font-bold text-white  hover:text-blue-600">
+                  sucess is not a Good Teacher, failuire makes you humble
+                </span>
+              </div>
+            </Link>
+
+            <Link to="">
+              <div className=" hover:text-blue-600">
+                <p className="text-xs"> oct 29, 2019 </p>
+                <span className="font-bold text-white  hover:text-blue-600">
+                  enot who has much is rich, but who gives much
+                </span>
+              </div>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
