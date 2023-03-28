826962c0a7528d392e2c2617cc6a35c1df5212aa
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-03-28 11:00:26 +0100

    still working on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="bg-black h-96">
       <div className="grid grid-col-4 w-full p-20">
         <div className="col-span-1">
-          <div className=" ">
+          <div className="">
             <p>Most Viewed Post </p>
           </div>
         </div>
