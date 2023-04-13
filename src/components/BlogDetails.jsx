b228aa8125a0a198f145fddb1d34ceb69435ce3c
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-04-13 12:46:25 +0100

    working on th blog detials

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_NEWS } from "../newsQueries";
+import Socials from "./Socials";
+import { Lightning } from "phosphor-react";
 
 const BlogDetails = () => {
   const { id } = useParams();
@@ -27,13 +29,39 @@ const BlogDetails = () => {
   console.log(dat && dat[0]);
 
   return (
-    <section className="grid md:grid-cols-3 gap-8 mt-20">
+    <section className="grid md:grid-cols-3 gap-8  md:p-16">
       <div className="md:col-span-2">
-        <div className="p-4 border border-indigo-500/20 ">
-          <div className="bg-gray-100 p-6 rounded-lg">
-            <h2 className="text-4xl capitalize text-gray-900 font-medium title-font mb-4">
-              {dat && dat[0].heading}
-            </h2>
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center gap-1 text-sm">
+            <p className="text-white bg-yellow-700 p-1 rounded-md px-2 ">
+              Life Style
+            </p>
+            <p className="text-white bg-blue-700 p-1 rounded-md px-3">Travel</p>
+            <p className="text-white bg-blue-600 p-1 rounded-md px-3">World</p>
+          </div>
+          <div className="flex gap-2 items-center font-bold">
+            <p className="bg-red-500 h-7 w-7 rounded-full items-center justify-center flex ">
+              <Lightning size={18} color="#ffffff" />
+            </p>
+            Trending
+          </div>
+          <h2 className="text-5xl capitalize text-gray-900 font-medium title-font ">
+            {dat && dat[0].heading}
+          </h2>
+          <h1 className="text-2xl text-gray-2 f">
+            Ten years after its formation
+          </h1>
+
+          <div>
+            <img
+              src="/public/as.png"
+              alt="pro"
+              className="rounded-full h-10 w-10 object-cover"
+            />
+          </div>
+        </div>
+        <div className=" border border-indigo-500/20 ">
+          <div className="bg-gray-100 rounded-lg">
             <img
               className="md:h-[500px] rounded w-full object-cover object-top mb-6"
               src={dat && dat[0].image.url}
@@ -54,7 +82,9 @@ const BlogDetails = () => {
 
       {/* section part of  */}
 
-      <div className="col-span-1"></div>
+      <div className="col-span-1">
+        <Socials />
+      </div>
     </section>
   );
 };
